2a5fb84d53d19a11b160a4c8f50f178efc76360d
LiamCodey <Liamcaramac@gmail.com>
2026-05-29 01:20:03 +0800

    Delete phish.js

--- phish.js
+++
@@ -1,9 +0,0 @@
-function goToRegister(){
-  document.getElementById("homeScreen").classList.remove("active");
-  document.getElementById("registerScreen").classList.add("active");
-}
-
-function goBack(){
-  document.getElementById("registerScreen").classList.remove("active");
-  document.getElementById("homeScreen").classList.add("active");
-}
